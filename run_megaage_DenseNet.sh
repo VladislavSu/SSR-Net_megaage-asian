82976735e46eaaa6d139a1568601d15515a3ba92
Vlad <puch@protonmail.com>
2019-11-22 23:31:27 +0200

    DenseNet fix

--- run_megaage_DenseNet.sh
+++ run_megaage_DenseNet.sh
@@ -1,2 +1,2 @@
-KERAS_BACKEND=tensorflow python3 ./MobileNet_and_DenseNet/TYY_train_others.py --input1 ./data/megaage_train.npz --input2 ./data/megaage_test.npz --netType 4 --db megaage --batch_size 50
+KERAS_BACKEND=tensorflow python3 ./MobileNet_and_DenseNet/TYY_train_others.py --input1 ./data/age_dataset_train.npz --input2 ./data/age_dataset_test.npz --netType 4 --db megaage --batch_size 50
 
